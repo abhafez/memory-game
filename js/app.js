ffe464b7e6aa54c4758603d1632dc9bab8d611ec
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-03-14 20:38:22 +0200

    Flip two cards with correct check function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,101 @@
-/*
- * Create a list that holds all of your cards
- */
+var cards = document.querySelectorAll(".card");
+
+var cardIcons = [
+    "fa-diamond",
+    "fa-paper-plane-o",
+    "fa-anchor",
+    "fa-bolt",
+    "fa-cube",
+    "fa-leaf",
+    "fa-bicycle",
+    "fa-bomb"
+];
+
+var twoCards = [];
+var twoCells = [];
+var count = 0;
+
+document.querySelector(".restart").addEventListener("click", startNewGame, false);
+
+// removeMe: (open show match).
+function startNewGame() {
+    let gameOver = false,
+        pair = [],
+        match = [],
+        newOrder = shuffle(cardIcons.concat(cardIcons));
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].childNodes[1].classList.value = "fa " + newOrder[i];
+    }
+
+    showAllCards();
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    setTimeout(() => {
+        flipBackAll();
+        play();
+    }, 2000);
+    // todo: make 6000 before submit.
+}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+function play() {
+    cards.forEach(function (card) {
+        card.addEventListener("click", showCard, false);
+    })
+}
+
+function showCard(cell) {
+    cell.target.classList.value = "card open show";
+    cell.target.removeEventListener("click", showCard, false);
+    twoCells.push(cell);
+    twoCards.push(cell.target.childNodes[1].classList[1]);
+    if (twoCards.length === 2) checkTwoCards();
+}
+
+function checkTwoCards() {
+    if (twoCards[0] === twoCards[1]) {
+        twoCells.forEach(function (cell) {
+            cell.target.classList.value = "card open show match";
+            twoCards = [];
+            twoCells = [];
+            count += 1;
+        });
+    } else {
+
+        twoCells.forEach(function (cell) {
+            setTimeout(() => {
+                cell.target.addEventListener("click", showCard, false);
+                cell.target.classList.value = "card";
+            }, 500); {}
+            twoCards = [];
+            twoCells = [];
+            count += 1;
+        });
+    }
+}
+
+function flipBackTwo() {
+
+}
+
+
+function showAllCards() {
+    // This function is used in every new game for 6 seconds
+    for (let card of cards) {
+        card.classList.value = "card show";
+    }
+}
+
+function flipBackAll() {
+    // Hide all cards to start the game.
+    for (const card of cards) {
+        console.log(card.classList.value = "card");
+    }
+}
+
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,15 +107,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
